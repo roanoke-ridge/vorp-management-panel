bc769ef79b3140263600495d39b0bf0b47f69368
Rob Danielson <31169893+rawb0t@users.noreply.github.com>
2024-10-28 23:19:32 -0500

    forgot to remove old db adapter artifacts

--- src/components/layout/user-avatar.tsx
+++ src/components/layout/user-avatar.tsx
@@ -1,4 +1,3 @@
-import { type User } from "@prisma/client"
 import { type AvatarProps } from "@radix-ui/react-avatar"
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
@@ -6,7 +5,10 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { User as UserIcon } from "lucide-react"
 
 interface UserAvatarProps extends AvatarProps {
-  user: Pick<User, "image" | "name">
+  user: {
+    image?: string | null;
+    name: string | null;
+  };
 }
 
 export function UserAvatar({ user, ...props }: UserAvatarProps) {
